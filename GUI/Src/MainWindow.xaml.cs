d9d4efe445ec7d3862f23e9fcc7631f29ae6883b
LinhengXilan <2642372751@qq.com>
2026-01-09 18:12:18 +0800

    调整界面控件布局

--- GUI/Src/MainWindow.xaml.cs
+++ GUI/Src/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
 // @file MainWindow.xaml.cs
-// @version 0.0.0.3
-// @date 2026-1-8
+// @version 0.0.0.4
+// @date 2026-1-9
 
 using ImageCLR;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-
 
 namespace GUI
 {
@@ -20,9 +19,39 @@ namespace GUI
 		public MainWindow() {
 			InitializeComponent();
 		}
+		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e) {
+			TextBoxFileOpened.Text = "当前打开文件：";
+		}
 		private void MainWindow_OnClosed(object sender, EventArgs e) {
 			m_Timer?.Stop();
 			m_Timer?.Dispose();
+		}
+		private void OpenFileDialog_OnClick(object sender, RoutedEventArgs e) {
+			var dialog = new Microsoft.Win32.OpenFileDialog {
+				FileName = String.Empty,
+				Filter = "图像文件|*.jpg;*.png;*.bmp"
+			};
+			var result = dialog.ShowDialog();
+
+			if (result != true) {
+				return;
+			}
+
+			try {
+				TextBoxFileOpened.Text = "当前打开文件：" + dialog.FileName;
+				m_Image.LoadImage(dialog.FileName);
+				m_Wb = new WriteableBitmap(m_Image.Width, m_Image.Height, 96, 96, PixelFormats.Bgr24, null);
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message, "无法加载图片");
+			}
+
+			ImagePending.Width = m_Image.Width;
+			ImagePending.Height = m_Image.Height;
+			ImagePending.Source = m_Wb;
+			RenderTransformPendingScale.ScaleX = 0.5;
+			RenderTransformPendingScale.ScaleY = 0.5;
+			m_Timer.Elapsed += (s, e) => RefreshFrame();
+			m_Timer.Start();
 		}
 		private void RefreshFrame() {
 			Array bufferPtr = m_Image.Data;
@@ -31,29 +60,6 @@ namespace GUI
 				m_Wb.WritePixels(new Int32Rect(0, 0, m_Wb.PixelWidth, m_Wb.PixelHeight), bufferPtr, m_Image.Stride, 0);
 				m_Wb.Unlock();
 			});
-		}
-
-		private void OpenFileDialog_OnClick(object sender, RoutedEventArgs e) {
-			var dialog = new Microsoft.Win32.OpenFileDialog();
-			dialog.FileName = String.Empty;
-			dialog.Filter = "图像文件|*.jpg;*.png;*.bmp";
-			var result = dialog.ShowDialog();
-			if (result == true) {
-				try {
-					Textbox_FilePath.Text = dialog.FileName;
-					m_Image.LoadImage(dialog.FileName);
-					m_Wb = new WriteableBitmap(m_Image.Width, m_Image.Height, 96, 96, PixelFormats.Bgr24, null);
-				} catch (Exception ex) {
-					MessageBox.Show(ex.Message, "无法加载图片");
-				}
-				Image_Pending.Width = m_Image.Width;
-				Image_Pending.Height = m_Image.Height;
-				Image_Pending.Source = m_Wb;
-				Image_PendingScale.ScaleX = 0.5;
-				Image_PendingScale.ScaleY = 0.5;
-				m_Timer.Elapsed += (s, e) => RefreshFrame();
-				m_Timer.Start();
-			}
 		}
 	}
 }
